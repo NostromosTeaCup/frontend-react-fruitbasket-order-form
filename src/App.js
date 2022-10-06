f41262eab1a3b83027e6c66cb9439ed035f6abb4
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-10-06 09:17:51 +0200

    basis voor fruitcounters

--- src/App.js
+++ src/App.js
@@ -5,6 +5,33 @@ function App() {
   return (
     <>
       <h1>Fruitmand bezorgservice</h1>
+        <section className="fruit-counters">
+            <article>
+               <h2>🍓 Aardbeien</h2>
+               <button className="button-minus">-</button>
+                <p>aantal</p>
+                <button className="button-plus">+</button>
+            </article>
+            <article>
+                <h2>🍌 Bananen</h2>
+                <button className="button-minus">-</button>
+                <p>aantal</p>
+                <button className="button-plus">+</button>
+            </article>
+            <article>
+                <h2>🍏 Appels</h2>
+                <button className="button-minus">-</button>
+                <p>aantal</p>
+                <button className="button-plus">+</button>
+            </article>
+            <article>
+                <h2>🥝 Kiwi's</h2>
+                <button className="button-minus">-</button>
+                <p>aantal</p>
+                <button className="button-plus">+</button>
+            </article>
+            <button>reset</button>
+        </section>
     </>
   );
 }
